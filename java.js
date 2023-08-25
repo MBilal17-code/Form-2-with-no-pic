fb8f4752cfe752cf801e91b5442f5f7d309c20b7
MBilal17-code <malikbilalbf7@gmail.com>
2023-08-25 17:35:37 +0500

    Sample

---
+++ java.js
@@ -0,0 +1,6 @@
+const Click = ()=>{
+  let email=document.getElementById("email").value;
+  let password=document.getElementById("password").value;
+  console.log(email);
+  console.log(password);
+}
